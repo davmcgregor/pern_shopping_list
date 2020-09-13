bbf0400019fa302db9b9198c8dcbb8178cc6d7c1
David McGregor <davidjohnmcgregor@gmail.com>
2020-09-13 15:14:04 +0100

    Add users model

--- database.sql
+++ database.sql
@@ -6,10 +6,15 @@ CREATE TABLE items (
     created_at timestamptz DEFAULT Now()
 );
 
-CREATE TABLE items (
-    item_id SERIAL PRIMARY KEY,
-    item_name VARCHAR(255) NOT NULL,
-    created_at timestamptz DEFAULT Now()
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    user_id uuid DEFAULT uuid_generate_v4(),
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL,
+    registered_at timestamptz DEFAULT Now(),
+    PRIMARY KEY(user_id)
 );
 
 INSERT INTO items (item_name) VALUES ('apples');
