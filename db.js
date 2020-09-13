89758037266469e3f384d52ea45b9cb247045020
David McGregor <davidjohnmcgregor@gmail.com>
2020-09-13 13:36:28 +0100

    Update database URL

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 const Pool = require("pg").Pool
 
-const pool = new Pool({
+const pool = new Pool(
+    process.env.DATABASE_URL ?
+    {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+          }
+    }
+    :
+    {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
