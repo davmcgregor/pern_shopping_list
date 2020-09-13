71a52017714a28f37bd0be59154b10425d25911b
David McGregor <davidjohnmcgregor@gmail.com>
2020-09-13 13:20:01 +0100

    Set up for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser')
+const path = require('path');
 const cors = require("cors");
 
 const app = express();
@@ -12,6 +13,15 @@ app.use(cors())
 // Routes
 app.use("/api/items", require("./routes/api/items"))
 
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
